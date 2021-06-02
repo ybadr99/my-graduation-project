f421f8147349fc27259639e2e9d07f0a26c666c9
ybadr99 <yosefbadr909@gmail.com>
2021-06-02 04:13:03 +0200

    arduino code

---
+++ NodeMCU_RFIDv2.0/NodeMCU_RFIDv2.0.ino
@@ -0,0 +1,144 @@
+#include <SPI.h>
+#include <MFRC522.h>
+//NodeMCU--------------------------
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+
+#define SS_PIN  2
+#define RST_PIN 0
+
+//************************************************************************
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance.
+//************************************************************************
+
+/* Set these to your desired credentials. */
+const char *ssid = "Home";
+const char *password = "Home20000";
+const char* device_token  = "a7ec812d2bf1866b";
+//************************************************************************
+String URL = "http://192.168.1.6/rfidattendance/getdata.php"; //computer IP or the server domain
+String getData, Link;
+String OldCardID = "";
+unsigned long previousMillis = 0;
+//************************************************************************
+double temp;
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  SPI.begin();  // Init SPI bus
+  mfrc522.PCD_Init(); // Init MFRC522 card
+  mlx.begin();
+  //---------------------------------------------
+  connectToWiFi();
+}
+//************************************************************************
+void loop() {
+  //check if there's a connection to Wi-Fi or not
+  if(!WiFi.isConnected()){
+    connectToWiFi();    //Retry to connect to Wi-Fi
+  }
+  //---------------------------------------------
+  if (millis() - previousMillis >= 15000) {
+    previousMillis = millis();
+    OldCardID="";
+  }
+  delay(50);
+  //---------------------------------------------
+  //look for new card
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;//got to start of loop if there is no card present
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;//if read card serial(0) returns 1, the uid struct contians the ID of the read card.
+  }
+  String CardID ="";
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    CardID += mfrc522.uid.uidByte[i];
+  }
+  //---------------------------------------------
+  if( CardID == OldCardID ){
+    return;
+  }
+  else{
+    OldCardID = CardID;
+  }
+  //---------------------------------------------
+//  Serial.println(CardID);
+  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempC()); 
+  Serial.print("*C\tObject = "); Serial.print(mlx.readObjectTempC()+7); Serial.println("*C");
+  temp = mlx.readObjectTempC()+7;
+  Serial.println();
+  SendCardID(CardID);
+  delay(1000);
+}
+//************send the Card UID to the website*************
+void SendCardID( String Card_uid ){
+  Serial.println("Sending the Card ID");
+  if(WiFi.isConnected()){
+    HTTPClient http;    //Declare object of class HTTPClient
+    //GET Data    
+    getData = "?card_uid=" + String(Card_uid) + "&device_token=" + String(device_token) + "&temp_degree=" + temp; // Add the Card ID to the GET array in order to send it
+    //GET methode
+    Link = URL + getData;
+    http.begin(Link); //initiate HTTP request   //Specify content-type header
+    
+    int httpCode = http.GET();   //Send the request
+    String payload = http.getString();    //Get the response payload
+
+//    Serial.println(Link);   //Print HTTP return code
+    Serial.println(httpCode);   //Print HTTP return code
+    Serial.println(Card_uid);     //Print Card ID
+    Serial.println(payload);    //Print request response payload
+
+    if (httpCode == 200) {
+      if (payload.substring(0, 5) == "login") {
+        String user_name = payload.substring(5);
+    //  Serial.println(user_name);
+
+      }
+      else if (payload.substring(0, 6) == "logout") {
+        String user_name = payload.substring(6);
+    //  Serial.println(user_name);
+        
+      }
+      else if (payload == "succesful") {
+
+      }
+      else if (payload == "available") {
+
+      }
+      delay(100);
+      http.end();  //Close connection
+    }
+  }
+}
+//********************connect to the WiFi******************
+void connectToWiFi(){
+    WiFi.mode(WIFI_OFF);        //Prevents reconnection issue (taking too long to connect)
+    delay(1000);
+    WiFi.mode(WIFI_STA);
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("Connected");
+  
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+    
+    delay(1000);
+}
+//=======================================================================
