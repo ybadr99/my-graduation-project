19e82fe3f74e736ca4121ec46946d34966006332
ybadr99 <yosefbadr909@gmail.com>
2021-06-03 21:32:21 +0200

    export mysql file

--- rfidattendance.sql
+++ rfidattendance.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 06, 2020 at 03:47 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.3.1
+-- Generation Time: Jun 03, 2021 at 09:31 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -54,8 +53,15 @@ CREATE TABLE `devices` (
   `device_dep` varchar(20) NOT NULL,
   `device_uid` text NOT NULL,
   `device_date` date NOT NULL,
-  `device_mode` tinyint(1) NOT NULL DEFAULT '0'
+  `device_mode` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `devices`
+--
+
+INSERT INTO `devices` (`id`, `device_name`, `device_dep`, `device_uid`, `device_date`, `device_mode`) VALUES
+(1, 'D1', 'test', 'a7ec812d2bf1866b', '2021-06-01', 1);
 
 -- --------------------------------------------------------
 
@@ -66,16 +72,28 @@ CREATE TABLE `devices` (
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(30) NOT NULL DEFAULT 'None',
-  `serialnumber` double NOT NULL DEFAULT '0',
+  `serialnumber` double NOT NULL DEFAULT 0,
   `gender` varchar(10) NOT NULL DEFAULT 'None',
   `email` varchar(50) NOT NULL DEFAULT 'None',
   `card_uid` varchar(30) NOT NULL,
-  `card_select` tinyint(1) NOT NULL DEFAULT '0',
+  `card_select` tinyint(1) NOT NULL DEFAULT 0,
   `user_date` date NOT NULL,
   `device_uid` varchar(20) NOT NULL DEFAULT '0',
   `device_dep` varchar(20) NOT NULL DEFAULT '0',
-  `add_card` tinyint(1) NOT NULL DEFAULT '0'
+  `temp_degree` text NOT NULL,
+  `add_card` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `serialnumber`, `gender`, `email`, `card_uid`, `card_select`, `user_date`, `device_uid`, `device_dep`, `temp_degree`, `add_card`) VALUES
+(1, 'None', 0, 'None', 'None', '48237127165', 0, '2021-06-01', 'a7ec812d2bf1866b', 'test', '0', 0),
+(2, 'mahmoud', 4, 'Male', 'mo33@g', '205245171139', 0, '2021-06-01', 'a7ec812d2bf1866b', 'test', '30', 1),
+(3, 'mohamed ahmed', 3, 'Male', 'mo@g.com', '11251212219', 0, '2021-06-01', 'a7ec812d2bf1866b', 'test', '31', 1),
+(4, 'ahmed', 2, 'Male', 'ahmed@g.com', '26137172139', 0, '2021-06-01', 'a7ec812d2bf1866b', 'test', '32', 1),
+(5, 'yousef badr', 1, 'Male', 'yousef.keto5523@gmail.com', '2115172139', 1, '2021-06-01', 'a7ec812d2bf1866b', 'test', '33', 1);
 
 -- --------------------------------------------------------
 
@@ -93,8 +111,21 @@ CREATE TABLE `users_logs` (
   `checkindate` date NOT NULL,
   `timein` time NOT NULL,
   `timeout` time NOT NULL,
-  `card_out` tinyint(1) NOT NULL DEFAULT '0'
+  `card_out` tinyint(1) NOT NULL DEFAULT 0,
+  `temp_degree` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users_logs`
+--
+
+INSERT INTO `users_logs` (`id`, `username`, `serialnumber`, `card_uid`, `device_uid`, `device_dep`, `checkindate`, `timein`, `timeout`, `card_out`, `temp_degree`) VALUES
+(13, 'yousef badr', 1, '2115172139', 'a7ec812d2bf1866b', 'test', '2021-06-02', '02:38:26', '00:00:00', 0, 38),
+(14, 'mahmoud', 4, '205245171139', 'a7ec812d2bf1866b', 'test', '2021-06-02', '02:46:46', '02:46:48', 1, 35),
+(15, 'ahmed', 2, '26137172139', 'a7ec812d2bf1866b', 'test', '2021-06-02', '02:47:27', '02:51:31', 1, 35),
+(16, 'mahmoud', 4, '205245171139', 'a7ec812d2bf1866b', 'test', '2021-06-02', '02:49:26', '00:00:00', 0, 30.09),
+(17, 'mohamed ahmed', 3, '11251212219', 'a7ec812d2bf1866b', 'test', '2021-06-02', '02:49:35', '02:51:39', 1, 30.05),
+(18, 'mohamed ahmed', 3, '11251212219', 'a7ec812d2bf1866b', 'test', '2021-06-02', '02:51:47', '00:00:00', 0, 37.31);
 
 --
 -- Indexes for dumped tables
@@ -138,19 +169,19 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `devices`
 --
 ALTER TABLE `devices`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users_logs`
 --
 ALTER TABLE `users_logs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
